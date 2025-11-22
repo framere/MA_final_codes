023c69576a14a53b5dd0c6f5fbd0fd9b9bb95ac5
framere <francesco.mereto@rwth-aachen.de>
2025-11-22 08:45:58 +0100

    Update TEST_CWNO.jl

--- TEST_CWNO.jl
+++ TEST_CWNO.jl
@@ -546,9 +546,9 @@ function main(molecule::String, l::Integer, Naux::Integer, max_iter::Integer)
 end
 
 molecule_dict = Dict(
-    "formaldehyde" => 100
+    "formaldehyde" => 200
 )
-Nauxs = [1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400]
+Nauxs = [800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400]
 
 for mol in keys(molecule_dict)
     println("\n=== Running tests for molecule: $mol ===")
